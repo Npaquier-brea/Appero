bc6b0562fd04a462c21736fc324e8eaa3dc516f1
Nicolas PAQUIER-BREA <nicolas.paquier-brea@epita.fr>
2021-05-28 15:38:29 +0200

    nodeToList

--- osmnx_test.py
+++ osmnx_test.py
@@ -14,6 +14,8 @@ def save(graph, place):
 
     print('saved')
 
+
+
 def load(place):
     try:
         with open('data/' + place + '.obj', 'rb') as file:
@@ -25,13 +27,24 @@ def load(place):
     except:
         return None
 
+def NodesToList(G):
+    resList = []
+    nodes = list(G.nodes())
+    for (count, node) in enumerate(nodes):
+        resList.append((count, node))
+    return resList
+
 def main():
     #city = ox.geocode_to_gdf("Montreal",  network_type = "drive")
-    place = 'paris'
+    place = 'Rochefourchat'
     city = load(place)
 
     if city == None:
         city = ox.graph_from_place(place)
+        G = ox.utils_graph.remove_isolated_nodes(city)
+        list = NodesToList(G)
+        for (a, b) in list:
+            print((a, b))
         save(city, place)
     print(load)
     ox.plot_graph(city)
